39b2194b58347bb63bc82212ea043edb6426d74d
zhang <zhenxing@zhang.com>
2023-07-12 21:14:44 +0800

    second commit(master分支)

--- src/main/java/com/zzx/HelloGit.java
+++ src/main/java/com/zzx/HelloGit.java
@@ -3,5 +3,6 @@ package com.zzx;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello git!");
     }
 }
